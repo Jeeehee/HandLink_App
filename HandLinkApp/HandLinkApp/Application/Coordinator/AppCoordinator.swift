f0065818fc51333bbf70a0dbaf011aa80b0bdb41
Dihee <hwangjihee94@gmail.com>
2023-01-26 15:50:27 +0900

    feat: #5 BaseCoordinator Delete Method 구현

---
+++ HandLinkApp/HandLinkApp/Application/Coordinator/AppCoordinator.swift
@@ -0,0 +1,70 @@
+//
+//  AppCoordinator.swift
+//  HandLinkApp
+//
+//  Created by Jihee hwang on 2022/11/24.
+//
+
+import UIKit
+import RxSwift
+import RxRelay
+
+final class AppCoordinator: BaseCoordinator, Navigation {
+    private let disposeBag = DisposeBag()
+    
+    var childCoordinators = [BaseCoordinator]()
+    var navigationController: UINavigationController
+    
+    // MARK: Navigation
+    var didTapNextButton = PublishRelay<Void>()
+    let diContainer: DIContainer
+    
+    
+    init(navigationController: UINavigationController, diContainer: DIContainer) {
+        self.navigationController = navigationController
+        self.diContainer = diContainer
+        
+        bind()
+    }
+    
+    func start() {
+        let child = FirstOnBoardingCoordinator(navigationController: navigationController, navigation: self)
+        addChild(child)
+        child.parentCoordinator = self
+        child.navigation = self
+        child.start()
+    }
+}
+
+// MARK: Private Function
+extension AppCoordinator {
+    private func bind() {
+        didTapNextButton
+            .bind { self.showSecondOnBoardingView() }
+            .disposed(by: disposeBag)
+    }
+    
+    private func showSecondOnBoardingView() {
+        removeFirstOnBoardingCoordinator()
+        
+        let child = SecondOnBoardingCoordinator(navigationController: navigationController, navigation: self)
+        addChild(child)
+        child.parentCoordinator = self
+        child.navigation = self
+        child.start()
+    }
+    
+    private func showHomeView() {
+        removeSecondOnBoardingCoordinator()
+    }
+    
+    private func removeFirstOnBoardingCoordinator() {
+        let removeCoordinator = FirstOnBoardingCoordinator(navigationController: navigationController, navigation: self)
+        childDidFinish(removeCoordinator)
+    }
+    
+    private func removeSecondOnBoardingCoordinator() {
+        let removeCoordinator = SecondOnBoardingCoordinator(navigationController: navigationController, navigation: self)
+        childDidFinish(removeCoordinator)
+    }
+}
